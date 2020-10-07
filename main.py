dc306b66039f4810291418df5b58e6aff8b12f6c
Matias <maty.infante@gmail.com>
2020-10-07 16:32:09 +0000

    adding Pydoc like

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+#@matirinfante on Github
+#Taller práctica II - Informática -- Pablo VI
+#Oct. 2020
+
 """
 Cálculo de Kahoot Winners
-@matirinfante on Github
 
 El cálculo de los kahoot winners se basa en la cantidad de participantes que hubo en la sesión y el porcentaje de acierto promedio una vez finalizado el quiz. Este último solo será tomado en cuenta si es mayor igual al 60%.
-"""
 
+En el algoritmo están incluidos conceptos de variables, entrada de datos, estructura condicional simple y aritmética sencilla.
+"""
 
 def calcKahootWinners():
     kahootWinnersBase = int(input('Cuantos participaron?\n')) // 3
